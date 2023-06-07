8e6d9ac2d75e277cdb044ab32793b0b2c8800553
Elizabeth <mwendemuthusi6@gmail.com>
2023-06-07 17:18:15 +0300

    Feat: add tasks component and sample data of tasks to render

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Form from './Form'
+import Tasks from './Tasks';
 
-function Home() {
+function Home() {  
   return (
     <div className="home">
       <div className="container mx-auto p-5">
         <Form />
+        <Tasks/>
       </div>
     </div>
   );
---
+++ src/components/Tasks.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Tasks() {
+  const tasks = [
+    {
+      id: 1,
+      taskName: "Pray",
+      description: "For a good job",
+      dueDate: "2018-07-23",
+      priority: "Critical",
+    },
+    {
+      id: 2,
+      taskName: "Apply singing contest",
+      description: "Judge Boe is there",
+      dueDate: "2018-08-23",
+      priority: "Medium",
+    },
+    {
+      id: 3,
+      taskName: "House hunting",
+      description: "Mainly in Ngong Road",
+      dueDate: "2018-09-23",
+      priority: "Minor",
+    },
+  ];
+  return (
+    <div>Tasks</div>
+  )
+}
+
+export default Tasks
