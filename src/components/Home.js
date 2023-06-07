b7b8826c25f7999b6a11aa741f23cefaab8a5833
Elizabeth <mwendemuthusi6@gmail.com>
2023-06-07 17:05:46 +0300

    Feat: add task input form

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Form from './Form'
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home">
+      <div className="container mx-auto p-5">
+        <Form />
+      </div>
+    </div>
+  );
 }
 
 export default Home
