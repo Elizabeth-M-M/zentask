939c9ed5b751c7d8c9f83a0520df38a6962e43b2
Elizabeth <mwendemuthusi6@gmail.com>
2023-06-07 21:02:57 +0300

    Refactor: transferred all tasks data to its own component

---
+++ src/assets/Data.js
@@ -0,0 +1,24 @@
+export const allTasks = [
+  {
+    id: 1,
+    taskName: "Pray",
+    description: "For a good job",
+    dueDate: "2018-07-23",
+    priority: "Critical",
+  },
+  {
+    id: 2,
+    taskName: "Apply singing contest",
+    description: "Judge Boe is there",
+    dueDate: "2018-08-23",
+    priority: "Medium",
+  },
+  {
+    id: 3,
+    taskName: "House hunting",
+    description: "Mainly in Ngong Road",
+    dueDate: "2018-09-23",
+    priority: "Minor",
+  },
+];
+    
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,30 +1,9 @@
 import React from 'react'
 import TaskCard from './TaskCard';
+import { allTasks } from '../assets/Data';
 
 function Tasks() {
-  const tasks = [
-    {
-      id: 1,
-      taskName: "Pray",
-      description: "For a good job",
-      dueDate: "2018-07-23",
-      priority: "Critical",
-    },
-    {
-      id: 2,
-      taskName: "Apply singing contest",
-      description: "Judge Boe is there",
-      dueDate: "2018-08-23",
-      priority: "Medium",
-    },
-    {
-      id: 3,
-      taskName: "House hunting",
-      description: "Mainly in Ngong Road",
-      dueDate: "2018-09-23",
-      priority: "Minor",
-    },
-  ];
+ const tasks= allTasks
   return (
     <div className="p-5">
       <div className=" gap-4 md:flex">
