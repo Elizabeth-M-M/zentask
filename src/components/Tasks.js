1f3399dd6781b9706524810054e4011a5c38a3f6
Elizabeth <mwendemuthusi6@gmail.com>
2023-06-07 20:57:37 +0300

    Feat: added a tasks component that renders the tasks as task cards

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import TaskCard from './TaskCard';
 
 function Tasks() {
   const tasks = [
@@ -25,8 +26,41 @@ function Tasks() {
     },
   ];
   return (
-    <div>Tasks</div>
-  )
+    <div className="p-5">
+      <div className=" gap-4 md:flex">
+        <div>
+          <h2 class="uppercase text-gray-700 font-bold mb-2 text-2xl text-center">
+            Urgent
+          </h2>
+          {tasks
+            .filter((task) => task.priority === "Critical")
+            .map((task) => (
+              <TaskCard task={task} />
+            ))}
+        </div>
+        <div>
+          <h2 class="uppercase text-gray-700 font-bold mb-2 text-2xl text-center">
+            Medium
+          </h2>
+          {tasks
+            .filter((task) => task.priority === "Medium")
+            .map((task) => (
+              <TaskCard task={task} />
+            ))}
+        </div>
+        <div>
+          <h2 class="uppercase text-gray-700 font-bold mb-2 text-2xl text-center">
+            Low
+          </h2>
+          {tasks
+            .filter((task) => task.priority === "Minor")
+            .map((task) => (
+              <TaskCard task={task} />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Tasks
